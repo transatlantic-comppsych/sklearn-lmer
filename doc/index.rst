8af52b4c2fcc9591579ce0a7258227b7ccc3e61a
shotgunosine <adenosine@gmail.com>
2019-09-05 16:35:55 -0400

    Initial commit

---
+++ doc/index.rst
@@ -0,0 +1,49 @@
+.. project-template documentation master file, created by
+   sphinx-quickstart on Mon Jan 18 14:44:12 2016.
+
+Welcome to sklearn-lmer's documentation!
+============================================
+
+This project is a simple scikit-learn estimator wrapper around pymer4, which in turn is a wrapper on LME4
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: Getting Started
+
+   quick_start
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: Documentation
+
+   user_guide
+   api
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: Tutorial - Examples
+
+   auto_examples/index
+
+`Getting started <quick_start.html>`_
+-------------------------------------
+
+Information regarding the estimator and how to use it.
+
+`User Guide <user_guide.html>`_
+-------------------------------
+
+An example of narrative documentation.
+
+`API Documentation <api.html>`_
+-------------------------------
+
+An example of API documentation.
+
+`Examples <auto_examples/index.html>`_
+--------------------------------------
+
+A set of examples. It complements the `User Guide <user_guide.html>`_.
---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+"""A template for scikit-learn compatible packages."""
+
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# get __version__ from _version.py
+ver_file = os.path.join('sklmer', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+DISTNAME = 'sklearn-lmer'
+DESCRIPTION = 'Scikit-learn estimator wrappers for pymer4 wrapped LME4 mixed effects models'
+with codecs.open('README.rst', encoding='utf-8-sig') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'D. Nielson'
+MAINTAINER_EMAIL = 'dylan.nielson@gmail.com'
+URL = 'https://github.com/nimh-mbdu/sklearn-lmer'
+LICENSE = 'CC0'
+DOWNLOAD_URL = 'https://github.com/nimh-mbdu/sklearn-lmer'
+VERSION = __version__
+INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn', 'pandas']
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+               'Programming Language :: Python',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7']
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest',
+        'pytest-cov'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'numpydoc',
+        'matplotlib'
+    ]
+}
+
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      zip_safe=False,  # the package can run out of an .egg file
+      classifiers=CLASSIFIERS,
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE)
