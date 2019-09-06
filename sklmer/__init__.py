252039a3ccf84956d84446d47d8816ca5cb8c4e0
shotgunosine <adenosine@gmail.com>
2019-09-06 11:22:31 -0400

    fix version

--- sklmer/__init__.py
+++ sklmer/__init__.py
@@ -1,11 +1,9 @@
 from ._estimators import LmerRegressor
 #from ._template import TemplateClassifier
+from ._version import get_versions
 
-from ._version import __version__
+__version__ = get_versions()['version']
+del get_versions
 
 __all__ = ['LmerRegressor',
            '__version__']
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
